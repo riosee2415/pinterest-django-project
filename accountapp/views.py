51bd417729b09993302d94e67dfe9a7f6305a863
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-07-22 00:13:24 +0900

    deploy gunicorn install

--- accountapp/views.py
+++ accountapp/views.py
@@ -22,7 +22,7 @@ has_ownwership = [
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy("accountapp:hello_world")
+    success_url = reverse_lazy("home")
     template_name = "accountapp/create.html"
 
 
@@ -47,7 +47,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = "target_user"
     form_class = AccountUpdateForm
-    success_url = reverse_lazy("accountapp:hello_world")
+    success_url = reverse_lazy("home")
     template_name = "accountapp/update.html"
 
 
